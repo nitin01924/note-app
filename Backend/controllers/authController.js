3b04f003fd95c4998fc493e642f00f7ad004efd7
nitin kumar <nitin981275@gmail.com>
2026-03-15 15:54:18 +0530

    normalize email before storing user in database

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -3,23 +3,23 @@ import User from "../models/User.js";
 import generateToken from "../utils/generateToken.js";
 
 export const registerUser = asyncHandler(async (req, res) => {
-  const { name, password } = req.body;
-  const email = req.body.email.toLowerCase();
+  const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
     return res.status(400).json({
       message: "The data fields are empty",
     });
   }
+  const normalizedEmail = email.toLowerCase();
 
-  const existingUser = await User.findOne({ email });
+  const existingUser = await User.findOne({ email: normalizedEmail });
   if (existingUser) {
     return res.status(400).json({
       message: "User already exists",
     });
   }
 
-  const user = await User.create({ name, email, password });
+  const user = await User.create({ name, email: normalizedEmail, password });
 
   res.status(201).json({
     id: user._id,
