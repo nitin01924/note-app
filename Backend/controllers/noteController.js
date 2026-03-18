e703246c03600247b6a516506cd11f186a797796
nitin kumar <nitin981275@gmail.com>
2026-03-18 16:14:16 +0530

    implement secure deleteNote with ownership validation

--- Backend/controllers/noteController.js
+++ Backend/controllers/noteController.js
@@ -25,3 +25,26 @@ export const getNote = asyncHandler(async (req, res) => {
     data: notes,
   });
 });
+
+export const deleteNote = asyncHandler(async (req, res) => {
+  const note = await Note.findById(req.params.id);
+
+  if (!note) {
+    return res.status(404).json({
+      message: "Note not found",
+    });
+  }
+
+  if (note.user.toString() !== req.user._id.toString()) {
+    return res.status(401).json({
+      message: "Not authorized to delete this note",
+    });
+  }
+
+  await note.deleteOne();
+
+  // 4️⃣ Response
+  res.status(200).json({
+    message: "Note deleted successfully",
+  });
+});
--- Backend/routes/noteRoutes.js
+++ Backend/routes/noteRoutes.js
@@ -1,10 +1,15 @@
 import express, { Router } from "express";
-import { createNote, getNote } from "../controllers/noteController.js";
+import {
+  createNote,
+  deleteNote,
+  getNote,
+} from "../controllers/noteController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", protect, createNote);
 router.get("/", protect, getNote);
+router.delete("/", protect, deleteNote);
 
 export default router;
