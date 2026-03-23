9ee347fbbacb802b8d3aecdf9030c6dfae97f549
nitin kumar <nitin981275@gmail.com>
2026-03-23 21:49:17 +0530

    implement basic auth flow with conditional rendering for login and notes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,28 @@
-import Notes from "./pages/Notes";
+import Notes from "./pages/Notes.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import { useState } from "react";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const token = localStorage.getItem("token");
+
+  //  If logged in → show Notes
+  if (token) {
+    return <Notes />;
+  }
+
+  //  If not logged in → show Login/Register
   return (
     <div>
-      <Notes />
+      {isLogin ? <Login /> : <Register />}
+
+      <button onClick={() => setIsLogin(!isLogin)}>
+        {isLogin ? "Go to Register" : "Go to Login"}
+      </button>
     </div>
   );
 }
+
 export default App;
