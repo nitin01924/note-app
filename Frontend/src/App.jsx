8c08089ee04e3633e1500313eed885ee2ce0781e
nitin kumar <nitin981275@gmail.com>
2026-03-28 20:30:33 +0530

    implement backend-verified authentication using /me endpoint and user state

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,47 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Notes from "./pages/Notes.jsx";
-import ProtectedRoutes from "./components/ProtectedRoutes.jsx";
 
+//  FUNCTION - APP
 function App() {
-  const token = localStorage.getItem("token");
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        const res = await fetch("http://localhost:3000/api/auth/me", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!res.ok) throw new Error("Unauthorized");
+
+        const data = await res.json();
+        setUser(data.user);
+      } catch (error) {
+        localStorage.removeItem("token");
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
 
   return (
     <Routes>
-      <Route path="/" element={token ? <Navigate to="/notes" /> : <Login />} />
+      <Route path="/" element={user ? <Navigate to="/notes" /> : <Login />} />
       <Route path="/register" element={<Register />} />
-      <Route
-        path="/notes"
-        element={
-          <ProtectedRoutes>
-            {" "}
-            <Notes />
-          </ProtectedRoutes>
-        }
-      />
+      <Route path="/notes" element={user ? <Notes /> : <Navigate to="/" />} />
     </Routes>
   );
 }
