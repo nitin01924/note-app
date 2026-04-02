51a882d9f5bb36298d4f2a19668aa6d3673239bf
nitin kumar <nitin981275@gmail.com>
2026-04-02 11:11:48 +0530

    refactor(api): remove redundant token handling and standardize data usage across services

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,60 +2,57 @@ import { apiRequest } from "../utils/api";
 
 //CONNECT REGISTERUSER
 export const registerUser = async (data) => {
-  const data = await apiRequest("/auth/register", {
+  const result = await apiRequest("/auth/register", {
     method: "POST",
     body: JSON.stringify(data),
   });
 
-  return data;
+  return result;
 };
 
 // LOGIN USER
 export const loginUser = async (data) => {
-  const data = await apiRequest("/auth/login", {
+  const result = await apiRequest("/auth/login", {
     method: "POST",
     body: JSON.stringify(data),
   });
 
-  return data;
+  return result;
 };
 
 // CREATE NOTES (PUSH)
 export const createNotes = async (data) => {
-  const token = localStorage.getItem("token");
-  const data = await apiRequest("/notes", {
+  const result = await apiRequest("/notes", {
     method: "POST",
     body: JSON.stringify(data),
   });
 
-  return data;
+  return result;
 };
 
 // FETCH NOTES (GET)
 export const getNotes = async () => {
-  const token = localStorage.getItem("token");
-  const data = await apiRequest("/notes", {
+  const result = await apiRequest("/notes", {
     method: "GET",
   });
-  return data;
+  return result;
 };
 
 // DELETE NOTE
 export const deleteNote = async (id) => {
-  const token = localStorage.getItem("token");
-  const data = await apiRequest(`/notes/${id}`, {
+  const result = await apiRequest(`/notes/${id}`, {
     method: "DELETE",
   });
 
-  return data;
+  return result;
 };
 
 // UPDATE NOTE
 export const updateNote = async (id, notedata) => {
-  const data = await apiRequest(`/notes/${id}`, {
+  const result = await apiRequest(`/notes/${id}`, {
     method: "PUT",
     body: JSON.stringify(notedata),
   });
 
-  return data;
+  return result;
 };
