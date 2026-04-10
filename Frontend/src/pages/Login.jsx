ef255f5f6ecc632364e6d4c2f13dd63584256f25
nitin kumar <nitin981275@gmail.com>
2026-04-10 17:54:02 +0530

    add(pages): creating a new login page with a minimal UI

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,58 +1,62 @@
 import { useState } from "react";
 import { loginUser } from "../services/api";
 import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import Input from "../components/Input";
 
 function Login() {
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    try {
+      e.preventDefault();
 
-    if (!email || !password) {
-      alert("Please fill all fields");
-      return;
+      if (!email || !password) {
+        alert("Please fill all fields");
+        return;
+      }
+      const data = { email, password };
+      const res = await loginUser(data);
+
+      localStorage.setItem("token", res.token);
+      window.location.reload();
+      navigate("/notes");
+
+      console.log(res);
+    } catch (error) {
+      // error handling
+      console.error(error);
+    } finally {
+      setLoading(false); //make state:false of setloading
     }
-    const data = { email, password };
-    const res = await loginUser(data);
-
-    localStorage.setItem("token", res.token);
-    window.location.reload();
-    navigate("/notes");
-
-    console.log(res);
   };
 
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-80 p-6 shadow-lg rounded-lg flex flex-col gap-4">
+        <h2 className="text-xl font-bold text-center">Login</h2>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Enter email"
+        <Input
+          label="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <br />
-        <br />
 
-        <input
+        <Input
+          label="Password"
           type="password"
-          placeholder="Enter password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <br />
-        <br />
 
-        <button type="submit">Login</button>
-      </form>
-      <p onClick={() => navigate("/register")}>
-        Don't have an account? Register
-      </p>
+        <Button onClick={handleSubmit} loading={loading}>
+          Login
+        </Button>
+      </div>
     </div>
   );
 }
