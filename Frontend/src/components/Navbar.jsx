86d1ad9f4828828763fc50ed62c8df0b09d42f42
nitin kumar <nitin981275@gmail.com>
2026-04-11 17:12:51 +0530

    feat: implement logout functionality and fix navbar button behavior

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import Button from "./Button";
+import { useNavigate } from "react-router-dom";
 
- function Navbar(params) {
-     return (
+function Navbar() {
+  const navigate = useNavigate();
+
+  return (
     <div className="flex justify-between items-center px-6 py-3 shadow">
       <h1 className="font-bold">Notes</h1>
-      <Button>Logout</Button>
+      <Button
+        onClick={() => {
+          localStorage.removeItem("token");
+          navigate("/");
+        }}
+      >
+        Logout
+      </Button>
     </div>
   );
-    
 }
+
 export default Navbar;
