a0d2c293c9e1bf235f42c984b0c3671e22258bec
nitin kumar <nitin981275@gmail.com>
2026-04-22 16:56:27 +0530

    style(navbar): update background color to dark gray

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar({ darkMode, setDarkMode, onLogout }) {
   const navigate = useNavigate();
 
   return (
-    <div className="flex justify-between items-center px-6 py-3 shadow bg-white dark:bg-gray-800 text-black dark:text-white">
+    <div className="flex justify-between items-center px-6 py-3 round border border-gray-700 shadow bg-white dark:bg-gray-950 text-black dark:text-white">
       <h1 className="font-bold">Notes</h1>
 
       <div className="flex gap-3">
